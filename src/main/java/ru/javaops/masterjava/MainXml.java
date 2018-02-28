569923fed75a2c16e542541f3580d2ec10998c59
Rimskiy <reomor@gmail.com>
2018-02-28 22:55:15 +0300

    refactor

--- src/main/java/ru/javaops/masterjava/MainXml.java
+++ src/main/java/ru/javaops/masterjava/MainXml.java
@@ -8,44 +8,47 @@ import ru.javaops.masterjava.xml.schema.User;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
 
+import javax.xml.validation.Schema;
+
+import java.io.InputStream;
+import java.net.URL;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class MainXml {
-    private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
+    public static void findUsersByProjectJaxb(String projectName, URL resourceUrl, Schema resourceSchema) throws Exception {
 
-    static {
-        JAXB_PARSER.setSchema(Schemas.ofClasspath("payload.xsd"));
-    }
-
-    public static Set<User> findUsersByProjectName(String projectName) throws Exception {
-        Payload payload = JAXB_PARSER.unmarshal(Resources.getResource("payload.xml").openStream());
-
-        Project project = payload.getProjects().getProject().stream()
-                .filter(proj -> proj.getName().equals(projectName))
-                .findAny()
-                .orElse(new Project());
-
-        final Set<String> projectGroupNames = project.getGroup().stream()
-                .map(Project.Group::getName)
-                .collect(Collectors.toSet());
+        final JaxbParser parserJaxb = new JaxbParser(ObjectFactory.class);
+        parserJaxb.setSchema(resourceSchema);
 
         Set<User> projectUsers = new TreeSet<>(Comparator.comparing(User::getEmail));
 
-        for (User user : payload.getUsers().getUser()) {
-            for (Object objectGroup : user.getGroups()) {
-                Project.Group group = (Project.Group) objectGroup;
-                if (projectGroupNames.contains(group.getName())) {
-                    projectUsers.add(user);
-                    break;
+        try (InputStream inputStream = resourceUrl.openStream()) {
+            Payload payload = parserJaxb.unmarshal(inputStream);
+            Project project = payload.getProjects().getProject().stream()
+                    .filter(proj -> proj.getName().equals(projectName))
+                    .findAny()
+                    .orElse(new Project());
+
+            final Set<String> projectGroupNames = project.getGroup().stream()
+                    .map(Project.Group::getName)
+                    .collect(Collectors.toSet());
+
+            for (User user : payload.getUsers().getUser()) {
+                for (Object objectGroup : user.getGroups()) {
+                    Project.Group group = (Project.Group) objectGroup;
+                    if (projectGroupNames.contains(group.getName())) {
+                        projectUsers.add(user);
+                        break;
+                    }
                 }
             }
         }
 
-        return projectUsers;
+        printSet(projectUsers);
     }
 
-    private static  <T extends User> void printSet(Set<T> set) {
+    private static <T extends User> void printSet(Set<T> set) {
         System.out.println("===");
         for (T element : set) {
             System.out.println(element.getEmail());
@@ -54,11 +57,11 @@ public class MainXml {
     }
 
     public static void main(String[] args) throws Exception {
-        final Set<User> usersTopjava = findUsersByProjectName("topjava");
-        printSet(usersTopjava);
-        final Set<User> usersBasejava = findUsersByProjectName("basejava");
-        printSet(usersBasejava);
-        final Set<User> usersMasterjava = findUsersByProjectName("masterjava");
-        printSet(usersMasterjava);
+        final URL resourceUrl = Resources.getResource("payload.xml");
+        final Schema resourceSchema = Schemas.ofClasspath("payload.xsd");
+
+        findUsersByProjectJaxb("topjava", resourceUrl, resourceSchema);
+        findUsersByProjectJaxb("basejava", resourceUrl, resourceSchema);
+        findUsersByProjectJaxb("masterjava", resourceUrl, resourceSchema);
     }
 }
